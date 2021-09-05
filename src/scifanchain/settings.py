38282668c04cd3afbfc7c2a5046e94eb9fd074ae
unity <unityoxb@gmail.com>
2021-09-06 00:09:56 +0800

    from server

--- src/scifanchain/settings.py
+++ src/scifanchain/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             # 'read_default_file': 'D:/scifanchain/web/src/scifanchain/db_dev.cnf',
-            'read_default_file': 'D:/scifanchain/web/src/scifanchain/db_prod.cnf',
+            'read_default_file': '/data/scifanchain/web/src/scifanchain/db_prod.cnf',
         },
     }
 }
