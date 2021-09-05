44a0541a421f3fe280374f1506646f5efa9c6451
unity <unityoxb@live.com>
2021-09-05 23:47:56 +0800

    change prod env

--- src/scifanchain/settings.py
+++ src/scifanchain/settings.py
@@ -23,6 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7-w$!n)j)ta+l%2x-&zy+$cup=+xmii=77jmxdmhe_34hn)-ar'
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -93,7 +96,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': 'D:/scifanchain/web/src/scifanchain/db_dev.cnf',
+            # 'read_default_file': 'D:/scifanchain/web/src/scifanchain/db_dev.cnf',
+            'read_default_file': 'D:/scifanchain/web/src/scifanchain/db_prod.cnf',
         },
     }
 }
