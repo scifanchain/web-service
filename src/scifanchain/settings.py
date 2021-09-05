d44884b4ab868e1b2721ef580b68c9e640a12432
unity <unityoxb@live.com>
2021-09-06 00:14:45 +0800

    up

--- src/scifanchain/settings.py
+++ src/scifanchain/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7-w$!n)j)ta+l%2x-&zy+$cup=+xmii=77jmxdmhe_34hn)-ar
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['118.195.181.77']
 
 # Application definition
 
