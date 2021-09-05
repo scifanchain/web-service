9eba408893d57538580032505b067ab94478951c
unity <unityoxb@gmail.com>
2021-09-06 00:46:53 +0800

    from server

---
+++ src/statics/js/index.js
@@ -0,0 +1,20 @@
+import {StageEditor, StageView} from './components'
+import ReactDOM from "react-dom";
+import React from "react";
+
+const stageEditorWrap = document.getElementById('StageEditorWrap')
+const stageViewWrap = document.getElementById('StageViewWrap')
+
+if (stageEditorWrap) {
+    ReactDOM.render(
+        <StageEditor/>,
+        document.getElementById('StageEditorWrap')
+    );
+}
+
+if (stageViewWrap) {
+    ReactDOM.render(
+        <StageView/>,
+        document.getElementById('StageViewWrap')
+    );
+}
