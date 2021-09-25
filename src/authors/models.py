0c568c11b4698aefaeab0dc6bd1e2a89e097319b
unity <unityoxb@gmail.com>
2021-09-26 00:36:50 +0800

    update models

--- src/authors/models.py
+++ src/authors/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Wallet(models.Model):
+    name = models.CharField(max_length=20)
     kind = models.CharField(max_length=50, default='')
     publickey = models.CharField(max_length=256)
     address = models.CharField(max_length=256)
